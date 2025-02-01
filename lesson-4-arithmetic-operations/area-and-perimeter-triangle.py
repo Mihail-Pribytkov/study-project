3eadfe263410d769d1c6ebbe8e2237338750c13c
Mihail Pribytkov <mihail.pribytkov@yandex.ru>
2025-02-01 21:26:58 +0300

    add perimetr and area program

---
+++ lesson-4-arithmetic-operations/area-and-perimeter-triangle.py
@@ -0,0 +1,12 @@
+print("Введите стороны прямоугольника")
+
+a, b, c, d = map(int, input().split())
+
+area = a * b;
+perimeter = ((a + b) * 2)
+
+print("Площадь прямоугльника равна:")
+print(area)
+
+print("Периметр прямоугольника равен:")
+print(perimeter)

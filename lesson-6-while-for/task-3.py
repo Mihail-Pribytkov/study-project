940fccc4a609a7956b39e23f6be71cc8cfbf2daa
Mihail Pribytkov <mihail.pribytkov@yandex.ru>
2025-02-08 16:32:26 +0300

    add task 3

---
+++ lesson-6-while-for/task-3.py
@@ -0,0 +1,14 @@
+# Задание №3
+
+# Вводятся целые числа A и B. Гарантируется, что A ≤ B. Выведите все четные числа на заданном отрезке через пробел.
+
+a, b = map(int, input("Введите целые числа A и B: ").split())
+
+even_numbers = []
+
+for num in range(a, b + 1):
+    if num % 2 == 0:
+        even_numbers.append(num)
+        
+output_string = " ".join(map(str, even_numbers))
+print(f"Четные числа в диапазоне от {a} до {b} = {even_numbers}")
